4687aadd114670cd8d4c5892d75c7796dbdde5b2
Your Name <romezde@gmail.com>
2025-08-15 14:29:33 +0800

    hosting trial

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './frontend/LandingPage';
 import './App.css';
 
